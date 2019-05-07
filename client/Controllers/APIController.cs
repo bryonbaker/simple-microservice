4f29b21483f1055bba70d93c8603ec68a79f525f
Bryon <bryonbakeraus@gmail.com>
2019-05-07 22:18:22 +1000

    Added client

---
+++ client/Controllers/APIController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CounterClaim.Models;
+
+namespace CounterClaim.Controllers
+{
+    public class APIController : Controller
+    {
+        public class Model
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+
+        public class Result
+        {
+            public int Id { get; set; }
+        }
+
+        [HttpPost]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public JsonResult Count([FromBody]Model model)
+        {
+            return Json(new Result { Id = (model.X + 1) * (model.Y + 1) });
+        }
+    }
+}
---
+++ client/wwwroot/js/site.js
@@ -0,0 +1,88 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function ready(fn) {
+    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+
+var maxX = 5;
+var maxY = 5;
+var cycleTimeout = 1000;
+
+
+function onready() {
+    let boxes = document.getElementById("boxes");
+    let x = 0;
+    let y = 0;
+
+    console.log('Starting');
+
+    for (x = 0; x < maxX; x++) {
+        let row = document.createElement("div");
+        boxes.append(row);
+        for (y = 0; y < maxY; y++) {
+            let box = document.createElement("div");
+            box.classList.add('box');
+            box.id = "_x" + x + "_y" + y;
+            box.innerText = box.id;
+            row.append(box);
+        }
+    }
+
+    let interval = setInterval(cycler, cycleTimeout);
+}
+
+var cycleX = 0;
+var cycleY = 0;
+function cycler() {
+    if (cycleX >= maxX) {
+        cycleX = 0;
+        cycleY++;
+    }
+
+    if (cycleY >= maxY) {
+        cycleY = 0;
+        cycleX = 0;
+    }
+
+    let boxId = "_x" + cycleX + "_y" + cycleY;
+    let box = document.getElementById(boxId);
+
+    query(box, cycleX, cycleY);
+
+    cycleX++;
+}
+
+function query(box, x, y) {
+    var request = new XMLHttpRequest();
+    box.classList.add('loading');
+    // request.open('POST', '/api/count', true);
+    request.open('POST', 'http://localhost:1234', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+
+    request.onload = function () {
+        if (request.status >= 200 && request.status < 400) {
+            // Success!
+            var data = JSON.parse(request.responseText);
+            box.innerText = data.id;
+            box.classList.remove('loading');
+        } else {
+            console.log('oops ' + request.statusText);
+
+        }
+    };
+
+    request.onerror = function () {
+        // There was a connection error of some sort
+    };
+
+    request.send(JSON.stringify({ x: x, y: y }));
+}
+
+ready(onready);
